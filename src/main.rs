13049d83855d6c318388d8e02276f8455e9feb31
Martijn Wallage <martijnwallage@gmail.com>
2026-09-03 15:17:09 +0100

    Add cities

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ enum Tile {
     Grass,
     Water,
     Mountain,
+    City,
+}
+
+struct City {
+    x: u16,
+    y: u16,
+    name: String,
 }
 
 struct AppState {
@@ -23,6 +30,7 @@ struct AppState {
     width: u16,
     height: u16,
     map: Vec<Vec<Tile>>,
+    cities: Vec<City>,
 }
 
 impl AppState {
@@ -41,7 +49,32 @@ impl AppState {
         map[7][11] = Tile::Mountain;
         map[6][11] = Tile::Mountain;
 
-        AppState { player_x: 5, player_y: 5, width, height, map }
+        AppState { player_x: 5, player_y: 5, width, height, map, cities: Vec::new() }
+    }
+
+    fn player_move(&mut self, dx: i16, dy: i16) {
+        let new_x = self.player_x as i32 + dx as i32;
+        let new_y = self.player_y as i32 + dy as i32;
+        if new_x >= 0 && new_x < self.width as i32 && new_y >= 0 && new_y < self.height as i32 {
+            if self.map[new_y as usize][new_x as usize] != Tile::Water {
+               self.player_x = new_x as u16;
+               self.player_y = new_y as u16;
+            }
+        } 
+    }
+
+    fn found_city(&mut self) {
+        let tile = self.map[self.player_y as usize][self.player_x as usize];
+        if tile != Tile::Grass {
+            return
+        }
+
+        self.map[self.player_y as usize][self.player_x as usize] = Tile::City;
+        self.cities.push(City {
+                x: self.player_x,
+                y: self.player_y,
+                name: String::from("Rome"),
+        });
     }
 }
 
@@ -58,6 +91,7 @@ fn render(state: &AppState) -> Paragraph<'static> {
                     Tile::Grass     => '.',
                     Tile::Mountain  => '▲',
                     Tile::Water     => '~',
+                    Tile::City      => '□',
                 };
                 lines.push(ch);
             }
@@ -86,10 +120,11 @@ fn main() -> io::Result<()> {
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q')  => break,
-                KeyCode::Up         => state.player_y = state.player_y.saturating_sub(1),
-                KeyCode::Down       => state.player_y = (state.player_y + 1).min(state.height - 1),
-                KeyCode::Left       => state.player_x = state.player_x.saturating_sub(1),
-                KeyCode::Right      => state.player_x = (state.player_x + 1).min(state.width - 1),
+                KeyCode::Char('b')  => state.found_city(),
+                KeyCode::Up         => state.player_move(0, -1),
+                KeyCode::Down       => state.player_move(0, 1),
+                KeyCode::Left       => state.player_move(-1, 0),
+                KeyCode::Right      => state.player_move(1, 0),
                 _                   => {}
             }
         }
